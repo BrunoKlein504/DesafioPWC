51fea5235c1c999d8bcf1b0f959035c48bd85399
BrunoKlein504 <bkleingomes@gmail.com>
2023-07-05 18:46:03 -0300

    Desafio 2 concluido

--- src/application/Challenge2.java
+++ src/application/Challenge2.java
@@ -3,8 +3,18 @@ package application;
 public class Challenge2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String input = "Hello, World!";
+		String output = input.toLowerCase();
+		StringBuilder final_output = new StringBuilder();
 
+		for (int i = 0; i < output.length(); i++) {
+			char letter = output.charAt(i);
+			if (letter == ' ' || output.indexOf(letter) == i) {
+				final_output.append(input.charAt(i));
+			}
+		}
+
+		System.out.println(final_output);
 	}
 
 }
