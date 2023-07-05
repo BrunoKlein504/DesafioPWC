e67d22ae637342adccd3f4fec9bcd85b60dde19e
BrunoKlein504 <bkleingomes@gmail.com>
2023-07-05 18:19:53 -0300

    desafio 1 feito

--- src/application/Challange1.java
+++ src/application/Challange1.java
@@ -3,8 +3,14 @@ package application;
 public class Challange1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String phrase = "Hello, World! OpenAI is amazing.";
+		String[] output = phrase.split(" ");
+		for (int i=output.length - 1; i>=0; i--) {
+			System.out.print(output[i]);
+			if (i > 0) {
+				System.out.print(" ");
+			}
+		}
 	}
 
 }
