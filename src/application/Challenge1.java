968bd6b1484da91c4d5bd58067eed143c50bdc4e
BrunoKlein504 <105749287+BrunoKlein504@users.noreply.github.com>
2023-07-06 16:56:17 -0300

    atualizado

--- src/application/Challenge1.java
+++ src/application/Challenge1.java
@@ -1,16 +1,23 @@
 package application;
 
+import java.util.Scanner;
+
 public class Challenge1 {
 
 	public static void main(String[] args) {
-		String phrase = "Hello, World! OpenAI is amazing.";
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a frase: ");
+		String phrase = sc.nextLine();
 		String[] output = phrase.split(" ");
-		for (int i=output.length - 1; i>=0; i--) {
+		for (int i = output.length - 1; i >= 0; i--) {
 			System.out.print(output[i]);
 			if (i > 0) {
 				System.out.print(" ");
 			}
 		}
+		sc.close();
 	}
 
 }
