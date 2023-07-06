436e42b1c90589f7a0f1890eb9dab1de40b4812b
BrunoKlein504 <105749287+BrunoKlein504@users.noreply.github.com>
2023-07-06 16:19:50 -0300

    desafio 5 concluido

--- src/application/Challenge5.java
+++ src/application/Challenge5.java
@@ -1,14 +1,42 @@
 package application;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Challenge5 {
 
 	public static void main(String[] args) {
-		
-		String input = "racecar";
-		StringBuilder result = new StringBuilder(input.toLowerCase());
-		boolean output = result.reverse().toString().equals(input.toLowerCase());
-		
-		System.out.println(output);
+		String input = "babad";
+		String largestPalindrome = findLargestPalindrome(input);
+		System.out.println("Largest palindrome: " + largestPalindrome);
 	}
 
+	private static String findLargestPalindrome(String input) {
+		// Remover caracteres não alfanuméricos e converter para letras minúsculas
+		String cleanedInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+		String largestPalindrome = "";
+		int maxLength = 0;
+
+		// Encontrar todos os substrings e verificar se são palíndromos
+		for (int i = 0; i < cleanedInput.length(); i++) {
+			for (int j = i + 1; j <= cleanedInput.length(); j++) {
+				String substring = cleanedInput.substring(i, j);
+				if (isPalindrome(substring) && substring.length() > maxLength) {
+					maxLength = substring.length();
+					largestPalindrome = substring;
+				}
+			}
+		}
+
+		return largestPalindrome;
+	}
+
+	private static boolean isPalindrome(String input) {
+		// Usar expressão regular para verificar se a string é um palíndromo
+		String reversed = new StringBuilder(input).reverse().toString();
+		Pattern pattern = Pattern.compile(reversed);
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
 }
