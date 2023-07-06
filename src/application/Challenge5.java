23def9c63f9e0f0009dc31c03f7f0da01a586a34
BrunoKlein504 <105749287+BrunoKlein504@users.noreply.github.com>
2023-07-06 17:01:00 -0300

    atualizado

--- src/application/Challenge5.java
+++ src/application/Challenge5.java
@@ -3,12 +3,25 @@ package application;
 public class Challenge5 {
 
 	public static void main(String[] args) {
-		
-		String input = "racecar";
-		StringBuilder result = new StringBuilder(input.toLowerCase());
-		boolean output = result.reverse().toString().equals(input.toLowerCase());
-		
-		System.out.println(output);
+
+		String word1 = "radar";
+		String word2 = "ana";
+		String word3 = "hello";
+		String word4 = "racecar";
+
+		System.out.println(verificarAnagramaPalindromo(word1)); // true
+		System.out.println(verificarAnagramaPalindromo(word2)); // true
+		System.out.println(verificarAnagramaPalindromo(word3)); // false
+		System.out.println(verificarAnagramaPalindromo(word4)); // true
 	}
 
+	public static boolean verificarAnagramaPalindromo(String palavra) {
+		String palavraInvertida = new StringBuilder(palavra).reverse().toString();
+
+		if (palavra.equals(palavraInvertida)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
